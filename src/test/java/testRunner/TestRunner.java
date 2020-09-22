73d512fff5056bc2d292316ca69807e1f0b93781
Anandhabadmanaban <anandha.srivi@gmail.com>
2020-09-22 12:37:48 +0530

    merging my code for runner

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
- //
+//if i want to keep my changes
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/java/features", glue="stepDefinations",plugin= {"pretty","html:target/cucumber","json:target/cucumber.json"})
 public class TestRunner extends AbstractTestNGCucumberTests  {
